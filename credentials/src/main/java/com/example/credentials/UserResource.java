142a6cf89ab25c109f47f3b46baf7342cd3d2c3f
Adham <adhamahmad541@gmail.com>
2023-04-20 03:43:55 +0200

    finished user register

--- credentials/src/main/java/com/example/credentials/UserResource.java
+++ credentials/src/main/java/com/example/credentials/UserResource.java
@@ -1,12 +1,11 @@
 package com.example.credentials;
 
 import jakarta.ejb.EJB;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.FormParam;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+
+import java.net.URI;
 
 @Path("/users")
 public class UserResource {
@@ -17,14 +16,27 @@ public class UserResource {
     @POST
     @Path("/register")
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public Response registerUser(
+    public Response registerUser(@FormParam("account-type") @DefaultValue("") String accountType,
                                  @FormParam("email") String email,
                                  @FormParam("password") String password) {
         User user = new User();
         user.setEmail(email);
         user.setPassword(password);
-        user.setAccountType("customer");
-        userRepository.createUser(user);
-        return Response.status(Response.Status.CREATED).build();
+        if(!accountType.isEmpty()){ // not customer signup
+            user.setAccountType(accountType);
+        }else{
+            user.setAccountType("customer");
+        }
+        
+        try {
+            userRepository.createUser(user);
+            Response response = Response.status(Response.Status.CREATED).build();
+            return response;
+        }
+        catch (Exception e){
+            //didn't create the user (duplicate emails)
+            URI uri = URI.create("http://localhost:8080/credentials-1.0-SNAPSHOT/hello-servlet");
+            return Response.seeOther(uri).build();
+        }
+       }
     }
-}
